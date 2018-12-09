580783335ce278b38d30c18234ad49ea88295e9a
Kelvin Lukorito <kelvin.lukorito@andela.com>
2018-12-09 13:11:43 +0300

    Add more methods

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,62 @@
+import hashlib
+import json
+from uuid import uuid4
+from time import time
+
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transaction = []
 
-    def new_block(self):
-        # creates a new block and adds to the chain
-        pass
+        # creates the genesis block
+        self.new_block(previous_hash=1, proof=100)
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates the Proof: Does hash(last_proof, proof) contains 4 leading zeroes
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+    def proof_of_work(self, last_proof):
+        """
+        Proof of work Algorithm:
+        - Find a number p such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+        - p is the previous proof, and p' is the new proof
+        :param last_proof: <int>
+        :return: <int>
+        """
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    def new_block(self, proof, previous_hash=None):
+        """
+        creates a new block and adds to the chain
+        :param proof: <int>
+        :return:
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[1])
+        }
+        # reset the current list of transactions
+        self.current_transaction = []
+
+        self.chain.append(block)
+        return block
 
     def new_transaction(self, sender, recipient, amount):
         """
@@ -21,12 +72,19 @@ class Blockchain(object):
             'amount': amount
         })
 
+        return self.last_block['index'] + 1
+
     @staticmethod
     def hash(block):
+        """
         # creates hashes for blocks
-        pass
+        :param block:  <dict> Block
+        :return: <str>
+        """
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdidest()
 
     @property
     def last_block(self):
         # return the last block of the chain
-        pass
+        return self.chain[-1]
